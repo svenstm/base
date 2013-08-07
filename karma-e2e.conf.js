7790849de4050dca5acda393ec842781a5fa499d
Cengizism <culusoy@ask-cs.com>
2013-08-07 14:07:55 +0200

    chore: fixed unit testing structure for travis

--- karma-e2e.conf.js
+++ karma-e2e.conf.js
@@ -1,59 +1,49 @@
-// Karma E2E configuration
+module.exports = function (karma)
+{
+  'use strict';
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+  karma.set({
+    frameworks: ['jasmine', 'ng-scenario'],
 
-// list of files / patterns to load in the browser
-files = [
-  'app/vendors/angular-scenario/angular-scenario.js',
-//  ANGULAR_SCENARIO,
-  ANGULAR_SCENARIO_ADAPTER,
-  'test/e2e/*.js'
-];
+    files: [
+      'app/vendors/angular-scenario/angular-scenario.js',
+      'test/e2e/*.js'
+    ],
 
-// list of files to exclude
-exclude = [];
+    basePath: '',
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = 'progress';
+    exclude: [],
 
-// web server port
-port = 8080;
+    reporters: ['progress'],
 
-// cli runner port
-runnerPort = 9100;
+    port: 8080,
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    runnerPort: 9100,
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    colors: true,
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
+    logLevel: karma.LOG_INFO,
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['PhantomJS'];
+    autoWatch: true,
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+    browsers: ['PhantomJS'],
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    captureTimeout: 5000,
 
-//Uncomment the following lines if you are using grunt's server to run the tests
-proxies = {
-   '/': 'http://localhost:9000/'
+    singleRun: false,
+
+    proxies: {
+      '/': 'http://localhost:9000/'
+    },
+
+    urlRoot: '/_karma_/',
+
+    plugins: [
+      'karma-jasmine',
+      'karma-ng-scenario',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-phantomjs-launcher'
+    ]
+  });
 };
-//URL root prevent conflicts with the site root
-urlRoot = '_karma_';
--- karma.conf.js
+++ karma.conf.js
@@ -1,71 +1,47 @@
 // Karma configuration
+module.exports = function (karma)
+{
+  'use strict';
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+  karma.set({
+    frameworks: ['jasmine', 'requirejs'],
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  REQUIRE,
-  REQUIRE_ADAPTER,
-  /**
-   *  !! Put all libs in RequireJS 'paths' config here (included: false).
-   *  All these files are files that are needed for the tests to run,
-   *  but Karma is being told explicitly to avoid loading them, as they
-   *  will be loaded by RequireJS when the main module is loaded.
-   */
+    files: [
+      {pattern: 'app/vendors/**/*.js',        included: false},
+      {pattern: 'app/scripts/*.js',           included: false},
+      {pattern: 'app/scripts/**/*.js',        included: false},
+      {pattern: 'test/spec/services/*.js',    included: false},
+      'test/spec/main.js'
+    ],
 
-  /**
-   *  All the sources, tests  // !! all src and test modules (included: false)
-   */
-  {pattern: 'app/vendors/**/*.js',        included: false},
-  {pattern: 'app/scripts/*.js',           included: false},
-  {pattern: 'app/scripts/**/*.js',        included: false},
-  {pattern: 'test/spec/services/*.js',    included: false},
+    basePath: '',
 
-  /**
-   * !! test main require module last
-   */
-  'test/spec/main.js'
-];
+    exclude: [],
 
-// list of files to exclude
-exclude = [];
+    reporters: ['progress'],
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = 'progress';
+    port: 8080,
 
-// web server port
-port = 8080;
+    runnerPort: 9100,
 
-// cli runner port
-runnerPort = 9100;
+    colors: true,
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    logLevel: karma.LOG_INFO,
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    autoWatch: true,
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
+    browsers: ['PhantomJS'],
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['PhantomJS'];
+    captureTimeout: 5000,
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+    singleRun: false,
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    plugins: [
+      'karma-jasmine',
+      'karma-requirejs',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-phantomjs-launcher'
+    ]
+  });
+};
